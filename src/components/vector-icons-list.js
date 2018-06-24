db9f09ce6f08f4f3e76cbddbef3b270155b85597
Andrey Parvatkin <5399514+badver@users.noreply.github.com>
2018-06-24 16:53:11 +0400

    add icon sets

---
+++ src/components/vector-icons-list.js
@@ -0,0 +1,25 @@
+import Entypo from '@expo/vector-icons/vendor/react-native-vector-icons/glyphmaps/Entypo.json';
+import EvilIcons from '@expo/vector-icons/vendor/react-native-vector-icons/glyphmaps/EvilIcons.json';
+import Feather from '@expo/vector-icons/vendor/react-native-vector-icons/glyphmaps/Feather.json';
+import FontAwesome from '@expo/vector-icons/vendor/react-native-vector-icons/glyphmaps/FontAwesome.json';
+import Foundation from '@expo/vector-icons/vendor/react-native-vector-icons/glyphmaps/Foundation.json';
+import Ionicons from '@expo/vector-icons/vendor/react-native-vector-icons/glyphmaps/Ionicons.json';
+import MaterialCommunityIcons from '@expo/vector-icons/vendor/react-native-vector-icons/glyphmaps/MaterialCommunityIcons.json';
+import MaterialIcons from '@expo/vector-icons/vendor/react-native-vector-icons/glyphmaps/MaterialIcons.json';
+import Octicons from '@expo/vector-icons/vendor/react-native-vector-icons/glyphmaps/Octicons.json';
+import SimpleLineIcons from '@expo/vector-icons/vendor/react-native-vector-icons/glyphmaps/SimpleLineIcons.json';
+import Zocial from '@expo/vector-icons/vendor/react-native-vector-icons/glyphmaps/Zocial.json';
+
+export default {
+  Entypo,
+  EvilIcons,
+  Feather,
+  FontAwesome,
+  Foundation,
+  Ionicons,
+  MaterialCommunityIcons,
+  MaterialIcons,
+  Octicons,
+  SimpleLineIcons,
+  Zocial,
+};
